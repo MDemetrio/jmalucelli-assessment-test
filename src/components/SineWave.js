f9f2047b38463c6786fe5dcd1d4c99aed5e17cb4
MDemetrio <mdemetrio.dev@gmail.com>
2018-10-13 18:43:36 -0300

    remove sinewave from test coverage and add comments to class

--- src/components/SineWave.js
+++ src/components/SineWave.js
@@ -1,10 +1,51 @@
-//The path animation code is an adaptation I (Matheus Demetrio) did from  Nick McMillan's CodePen https://codepen.io/hey-nick/pen/KVWvJv
+//This code is based on this Nick McMillan's CodePen https://codepen.io/hey-nick/pen/KVWvJv to animate a svg sine wave
+//It was coded imperatively and manipulate DOM directly, not using react features like state and virtual dom  
+//This is because it does not provide much value to this webapp objetive at the moment, but it could be refactored to a better, react compliant component. 
 
 import React, { Component } from 'react'
 
 const randomRange = function (min, max) {
     return ~~(Math.random() * (max - min + 1)) + min
 };
+
+const createPathString = function (path) {
+
+    var completedPath = '',
+        comma = ',',
+        ampl = 50; // pixel range from 0, aka how deeply they bend
+
+    var current = {
+        x: ampl * Math.sin(path.counter / path.sin),
+        y: ampl * Math.cos(path.counter / path.cos)
+    };
+
+    var newPathSection = 'M' +
+        // starting point
+        path.startX +
+        comma +
+        path.startY +
+        // quadratic control point
+        ' Q' +
+        path.pointX +
+        comma +
+        (current.y * 1.5).toFixed(3) + // 1.5 to amp up the bend a little
+        // center point intersection
+        ' ' +
+        ((current.x) / 10 + path.centerX).toFixed(3) +
+        comma +
+        ((current.y) / 5 + path.centerY).toFixed(3) +
+        // end point with quadratic reflection (T) (so the bottom right mirrors the top left)
+        ' T' +
+        path.endX +
+        comma +
+        path.endY;
+    path.counter++;
+
+    completedPath += newPathSection;
+
+
+    return completedPath;
+}
 
 export default class SineWave extends Component {
     constructor(props) {
@@ -13,47 +54,8 @@ export default class SineWave extends Component {
         this.divRef = React.createRef();
     }
 
-    createPathString = (path) => {
-
-        var completedPath = '',
-            comma = ',',
-            ampl = 50; // pixel range from 0, aka how deeply they bend
-
-        var current = {
-            x: ampl * Math.sin(path.counter / path.sin),
-            y: ampl * Math.cos(path.counter / path.cos)
-        };
-
-        var newPathSection = 'M' +
-            // starting point
-            path.startX +
-            comma +
-            path.startY +
-            // quadratic control point
-            ' Q' +
-            path.pointX +
-            comma +
-            (current.y * 1.5).toFixed(3) + // 1.5 to amp up the bend a little
-            // center point intersection
-            ' ' +
-            ((current.x) / 10 + path.centerX).toFixed(3) +
-            comma +
-            ((current.y) / 5 + path.centerY).toFixed(3) +
-            // end point with quadratic reflection (T) (so the bottom right mirrors the top left)
-            ' T' +
-            path.endX +
-            comma +
-            path.endY;
-        path.counter++;
-
-        completedPath += newPathSection;
-
-
-        return completedPath;
-
-    };
-
     createLines = () => {
+        //remove lines in case the window is resized, so they can be created again based on new size
         while (this.svgRef.current.firstChild) {
             this.svgRef.current.removeChild(this.svgRef.current.firstChild);
         }
@@ -67,18 +69,18 @@ export default class SineWave extends Component {
         let newPathEl2 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
         let newPathEl3 = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
-        // create an arr which contains objects for all lines
-        // createPathString() will use this array
         const currentWidth = this.divRef.current.clientWidth || 0;
         const currentHeigth = this.divRef.current.clientHeight || 0;
 
+        // create an arr which contains objects for all lines
+        // createPathString() will use this array
         for (var i = 0; i < numberOfLines; i++) {
 
             var lineDataObj = {
                 counter: randomRange(1, 1000), // a broad counter range ensures lines start at different cycles (will look more random)
                 startX: randomRange(-5, -40),
                 startY: randomRange((currentHeigth / 2) - 20, (currentHeigth / 2) + 20),
-                endX: randomRange(currentWidth, currentWidth + 20), // viewbox = 200
+                endX: randomRange(currentWidth, currentWidth + 20),
                 endY: randomRange((currentHeigth / 2) - 20, (currentHeigth / 2) + 20),
                 sin: randomRange(minSpeed, maxSpeed),
                 cos: randomRange(minSpeed, maxSpeed),
@@ -91,35 +93,35 @@ export default class SineWave extends Component {
         }
 
         const animLoop = () => {
-            newPathEl1.setAttribute('d', this.createPathString(lineDataArr[0]));
-            newPathEl2.setAttribute('d', this.createPathString(lineDataArr[1]));
-            newPathEl3.setAttribute('d', this.createPathString(lineDataArr[2]));
+            newPathEl1.setAttribute('d', createPathString(lineDataArr[0]));
+            newPathEl2.setAttribute('d', createPathString(lineDataArr[1]));
+            newPathEl3.setAttribute('d', createPathString(lineDataArr[2]));
             requestAnimationFrame(animLoop);
         }
 
-        //Add transition to ease path appearing in case of resize
-        newPathEl1.setAttribute("style", "transition: opacity .20s ease-in-out; - moz - transition: opacity .20s ease -in -out; -webkit - transition: opacity .20s ease -in -out; ");
-        newPathEl2.setAttribute("style", "transition: opacity .50s ease-in-out; - moz - transition: opacity .50s ease -in -out; -webkit - transition: opacity .50s ease -in -out; ");
-        newPathEl3.setAttribute("style", "transition: opacity .90s ease-in-out; - moz - transition: opacity .90s ease -in -out; -webkit - transition: opacity .90s ease -in -out; ");
+        //The transition smooths path appearing in case of resize
 
         newPathEl1.setAttribute('stroke', "#7d65ef");
         newPathEl1.setAttribute('stroke-width', "2");
         newPathEl1.setAttribute('opacity', "0");
+        newPathEl1.setAttribute("style", "transition: opacity .20s ease-in-out; - moz - transition: opacity .20s ease -in -out; -webkit - transition: opacity .20s ease -in -out; ");
 
         newPathEl2.setAttribute('stroke', "#9274ee");
         newPathEl2.setAttribute('stroke-width', "2");
         newPathEl2.setAttribute('opacity', "0");
+        newPathEl2.setAttribute("style", "transition: opacity .50s ease-in-out; - moz - transition: opacity .50s ease -in -out; -webkit - transition: opacity .50s ease -in -out; ");
 
         newPathEl3.setAttribute('stroke', "#ddd");
         newPathEl3.setAttribute('stroke-width', "1");
         newPathEl3.setAttribute('opacity', "0");
+        newPathEl3.setAttribute("style", "transition: opacity .90s ease-in-out; - moz - transition: opacity .90s ease -in -out; -webkit - transition: opacity .90s ease -in -out; ");
 
 
         this.svgRef.current.appendChild(newPathEl1);
         this.svgRef.current.appendChild(newPathEl2);
         this.svgRef.current.appendChild(newPathEl3);
 
-        //set opacity
+        //set opacity after appending, so it don't glitch visually in case of resize
         setTimeout(() => {
             newPathEl1.setAttribute('opacity', "1");
             newPathEl2.setAttribute('opacity', "1");
@@ -142,6 +144,7 @@ export default class SineWave extends Component {
 
     render() {
         return (
+            //height and width set on both div and svg as a workaround to firefox behaving differently from other browsers
             <div ref={this.divRef} style={{ height: '32vh', width: '100vw', overflow: 'hidden', position: 'absolute', top: 0, left: 0 }} aria-hidden={true}>
                 <svg style={{ height: '32vh', width: '100vw' }}
                     fill='none'
