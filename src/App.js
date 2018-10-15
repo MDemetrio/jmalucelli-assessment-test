c4f8aeb97a642f77a73ff857c923d780ac31a134
MDemetrio <mdemetrio.dev@gmail.com>
2018-10-15 18:46:34 -0300

    set accesstoken on App.js mount and add App.js snapshot tests

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 import Quote from './Quote';
 import Landing from './Landing';
-import { Transition, config } from 'react-spring'
 
 class App extends Component {
+  
+  componentDidMount() {
+    localStorage.setItem('ACCESS-TOKEN', 23456789)
+  }
+
   render() {
     return (
       <Router>
-        <Route
-          render={({ location }) => (
-
-            <Transition
-              config={config.slow}
-              keys={location.pathname}
-              from={{ opacity: 0, transform: 'translate(50px, 0px)' }}
-              enter={{ opacity: 1, transform: 'translate(0px, 0px)' }}
-              leave={{ opacity: 0, transform: 'translate(-400px, 0px)' }}>
-              {style => (
-                <Switch location={location}>
-                  <Route exact path="/" render={props => <Landing {...props} style={style} />} />
-                  <Route exact path="/quote" render={props => <Quote {...props} style={style} />} />
-                  <Redirect to="/" />
-                </Switch>
-              )}
-            </Transition>
-          )} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/quote" component={Quote} />
+          <Redirect to="/" />
+        </Switch>
       </Router>
     );
   }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import App from './App';
+import App from '../App';
 import { shallow, mount } from 'enzyme';
 import { MemoryRouter } from 'react-router';
-import Landing from './Landing';
-import Quote from './Quote';
+import Landing from '../Landing';
+import Quote from '../Quote';
 
 it('renders without crashing', () => {
-  shallow(<App />);
+  const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
 
-describe('Routing', () => {
+describe('App routing', () => {
   it('should render Landing component on "/" route', () => {
     const wrapper = mount(
       <MemoryRouter initialEntries={['/']}>
         <App />
       </MemoryRouter>
     )
-    expect(wrapper.containsMatchingElement(<Landing />)).toBe(true);
+    expect(wrapper.containsMatchingElement(Landing)).toBe(true);
   })
 
   it('should render Quote component on "/quote" route', () => {
@@ -25,7 +26,7 @@ describe('Routing', () => {
         <App />
       </MemoryRouter>
     )
-    expect(wrapper.containsMatchingElement(<Quote />)).toBe(true);
+    expect(wrapper.containsMatchingElement(Quote)).toBe(true);
   })
 
   it('should render Landing component on undeclared route', () => {
@@ -34,7 +35,7 @@ describe('Routing', () => {
         <App />
       </MemoryRouter>
     )
-    expect(wrapper.containsMatchingElement(<Landing />)).toBe(true);
+    expect(wrapper.containsMatchingElement(Landing)).toBe(true);
   })
 
 });
